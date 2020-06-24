66c7bbcf4cd23bfbe176c54a314fdb82ec888cf3
Bhumika Singh <bhumika.singh@synapse.com>
2020-06-25 02:59:11 +0530

    Application added successfully

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDeTa8hbcDun2Yy54nFQzbmMXRx1mu2yPw",
+    authDomain: "eshop-995dd.firebaseapp.com",
+    databaseURL: "https://eshop-995dd.firebaseio.com",
+    projectId: "eshop-995dd",
+    storageBucket: "eshop-995dd.appspot.com",
+    messagingSenderId: "1008135547439",
+    appId: "1:1008135547439:web:fa7e7320641f000b32f4da",
+    measurementId: "G-M1DJ9Q1VCK"
+  }
+};
